e05173e42b2052305f8a704cffeba624d49aa1ff
vad998 <57177263+vad998@users.noreply.github.com>
2021-10-16 18:20:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,18 @@ import os
 import socket
 
 app = Flask(__name__)
-c = 0
+counter = 0
 
 @app.route("/")
-def current_value_c():
-    return f"Current counter value: {c}"
+def current_value_counter():
+    return f"Current counter value: {counter}"
 
 @app.route("/stat")
-def increment_с():
-    global c
-    msg = f"Current counter value: {c}"
-    c += 1
-    return msg + f"\nThe counter value has been incremented."
+def increment_counter():
+    global counter
+    message = f"Current counter value: {counter}"
+    counter += 1
+    return message + f"\nThe counter value has been incremented."
 
 @app.route("/about")
 def hello():
